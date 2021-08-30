65629dbabd0ae2882fe312acfeac80e56a0f37e8
Kevin Escudero <kevin.escudero@ias.com.co>
2021-08-30 15:54:06 -0500

    React-Native-AUTOSCROLL-FLATLIST

--- example/expo/Colors.ts
+++
@@ -1,8 +0,0 @@
-export enum Colors {
-    TEAL_GREEN = "#006d5b",
-    WHITE = "#ffffff",
-    LIGHT_BLUE = "#add8e6",
-    PALE_GRAY = "#f1f1f1",
-    GAINSBORO = "#dcdcdc",
-    BLACK = "#000000",
-}
